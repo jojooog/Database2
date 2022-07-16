50ce298ef2e9a10f0da7a9c4d43dc9c304b1510c
jojooog <jooog0417@gmail.com>
2022-07-16 15:31:55 +0900

    jpa를 적용해서 item 객체와 테이블 매핑해보기

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -1,12 +1,17 @@
 package hello.itemservice.domain;
 
 import lombok.Data;
+import javax.persistence.*;
 
 @Data
+@Entity
 public class Item {
 
+    //id를 pk로 지정하고 디비에서 자동으로 값을 넣어주는 방식을 선택
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
