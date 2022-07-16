ec644564683d42286b99a2c94e6b75b2ff46ac62
jojooog <jooog0417@gmail.com>
2022-07-16 16:01:38 +0900

    jpa를 적용해서 repository 만들고 EntityManager 주입받아 사용하기

---
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
@@ -0,0 +1,53 @@
+package hello.itemservice.repository.jpa;
+
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+@Transactional
+@RequiredArgsConstructor
+public class JpaItemRepository implements ItemRepository {
+
+    private final EntityManager entityManager;
+
+    @Override
+    public Item save(Item item) {
+        entityManager.persist(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = entityManager.find(Item.class, itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        Item item = entityManager.find(Item.class, id);
+        return Optional.ofNullable(item);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String jpql = "select i from Item i";
+
+        List<Item> findAllItem = entityManager.createQuery(jpql, Item.class).getResultList();
+
+        return findAllItem;
+    }
+}
